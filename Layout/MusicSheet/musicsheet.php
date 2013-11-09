faae91f1f72f8f4949c8cee3a8d4e7a13ee35c4f
nicnhus <nicolas.husser@gmail.com>
2013-11-09 07:01:03 -0500

    Pushing js to main

---
+++ Layout/MusicSheet/musicsheet.php
@@ -0,0 +1,16 @@
+
+
+<?php #xml version = "1.0" encoding = "utf-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml1-strict.dtd">
+<html><head> <title> Music Sheet</title>
+ <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="jquery-ui-1.8.20.custom.min.js"></script> 
+<script type="text/javascript" src="scorediv-pv0.0.3.js"></script> 
+
+
+</head>
+<div class="score-div" style="position: relative; width: 700px; height: 500px;" musicxml_ref="testxml.xml"><canvas width="700" height="462" style="position: absolute; left: 0px; top: 0px;"></canvas></div>	
+	
+	
+</html>
