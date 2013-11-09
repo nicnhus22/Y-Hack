18121d9cea77cb24fbbfd800a629e65b4bac9bbd
Gabriel <belmonte.gabriel.k@gmail.com>
2013-11-08 21:21:47 -0500

    Example empy project

---
+++ Backend/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wolfram.NETLink;
+
+namespace TestMathematica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This launches the Mathematica kernel:
+            IKernelLink ml = MathLinkFactory.CreateKernelLink();
+
+            // Discard the initial InputNamePacket the kernel will send when launched.
+            ml.WaitAndDiscardAnswer();
+            var evalStr = @"Data = Import[""C:\Users\zhadowoflight\Desktop\Studies\COEN_9\y-hack\lotr.wav"" , ""DATA""] ";
+            ml.Evaluate( evalStr );
+            PacketType p = ml.WaitForAnswer();
+            try
+            {
+                string res = ml.GetSymbol();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
