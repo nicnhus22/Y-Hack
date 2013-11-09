f6f7cbd26097e261e5075948be6c75cb18bf6bab
syswsi <sy.sw.si@gmail.com>
2013-11-09 08:55:39 -0500

    updated Upload
    Added line to upload which runs the mathematica project

--- Layout/upload.php
+++ Layout/upload.php
@@ -2,4 +2,7 @@
 
 move_uploaded_file($_FILES['file']['tmp_name'], $target_path = $_SERVER['DOCUMENT_ROOT'] . "/Layout/" . "song.wav");
 
+echo exec("\Backend\bin\Debug\TestMathematica.vshost.exe $target_path 500 1.0"); 
+
+
 ?>
